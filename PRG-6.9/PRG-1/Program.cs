071630a9c6c9ac972a7c8d1597b7a031ad26a78a
Reischig Antonin <reischig@gymvod.local>
2024-09-18 11:59:56 +0200

    1. nahrani

---
+++ PRG-6.9/PRG-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 5.2f;
+            int b = 4;
+            float result = a / b;
+            char text = 'a';
+            Console.WriteLine(result);
+            Console.WriteLine(text);
+            Console.WriteLine("hello World");
+            Console.ReadKey();
+        }
+    }
+}

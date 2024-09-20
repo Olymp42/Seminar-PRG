577014f77b87cb272f0dceb3b830df295748109b
Reischig Antonin <reischig@gymvod.local>
2024-09-20 09:30:58 +0200

    neco

--- PRG-6.9/PRG-1/Program.cs
+++ PRG-6.9/PRG-1/Program.cs
@@ -10,13 +10,54 @@ namespace PRG_1
     {
         static void Main(string[] args)
         {
-            float a = 5.2f;
-            int b = 4;
-            float result = a / b;
-            char text = 'a';
-            Console.WriteLine(result);
+            Random rng = new Random();
+            for (int i = 10; i > 0; i--)
+            {
+                // Console.WriteLine(i);
+                int a = rng.Next(100);
+                if (a > 50)
+                {
+                    Console.WriteLine("Vetsi nez 50.");
+                }
+                else if( a > 25)
+                {
+                    Console.WriteLine("vetsi nez 25.");
+                }
+                else //a bude vetsi nebo rovno 50
+                {
+                    Console.WriteLine("Promena a byla mensi nebo rovna 25.");
+                }
+                Console.WriteLine(a);
+            }
+            bool canContinue = true;
+            do
+            {
+                int a = rng.Next(100);
+                Console.WriteLine(a);
+                string userInput = Console.ReadLine();
+                if (userInput == "n" || userInput == "N" || userInput == "x")// nebo ||, zaroven &&
+                {
+                    canContinue = false;
+                    Console.WriteLine("uzivatel chce skoncit");
+                }
+            } while (canContinue);
+            /*
+            string text;
+            int yesOrNO -rng.Next(100);
+            if (yesOrNO > 50)
+            {
+                text = "yes";
+            }
+            else
+            {
+                text = "yes";
+            }
+            */
+            string text = rng.Next(100) > 50 ? "yes" : "no";
+
             Console.WriteLine(text);
-            Console.WriteLine("hello World");
+            
+            
             Console.ReadKey();
         }
     }

f2e4969c03cd9db5329522a7391087fb6e3d26b9
Reischig Antonin <reischig@gymvod.local>
2025-03-28 09:34:55 +0100

    28.03.2025

---
+++ malovani/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace malovani
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonBLue = new System.Windows.Forms.Button();
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonGreen = new System.Windows.Forms.Button();
+            this.buttonYellow = new System.Windows.Forms.Button();
+            this.buttonBlack = new System.Windows.Forms.Button();
+            this.buttonLightBlue = new System.Windows.Forms.Button();
+            this.buttonOrange = new System.Windows.Forms.Button();
+            this.buttonLightGreen = new System.Windows.Forms.Button();
+            this.buttonPink = new System.Windows.Forms.Button();
+            this.buttonWhite = new System.Windows.Forms.Button();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.buttonErase = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 87);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(642, 312);
+            this.flowLayoutPanel1.TabIndex = 1;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            this.flowLayoutPanel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.flowLayoutPanel1_MouseDown);
+            this.flowLayoutPanel1.MouseHover += new System.EventHandler(this.flowLayoutPanel1_MouseHover);
+            this.flowLayoutPanel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.flowLayoutPanel1_MouseMove);
+            // 
+            // buttonBLue
+            // 
+            this.buttonBLue.BackColor = System.Drawing.Color.Blue;
+            this.buttonBLue.Location = new System.Drawing.Point(559, 17);
+            this.buttonBLue.Name = "buttonBLue";
+            this.buttonBLue.Size = new System.Drawing.Size(27, 23);
+            this.buttonBLue.TabIndex = 2;
+            this.buttonBLue.UseVisualStyleBackColor = false;
+            this.buttonBLue.Click += new System.EventHandler(this.buttonBLue_Click);
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.BackColor = System.Drawing.Color.Red;
+            this.buttonRed.Location = new System.Drawing.Point(592, 17);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(27, 23);
+            this.buttonRed.TabIndex = 3;
+            this.buttonRed.UseVisualStyleBackColor = false;
+            this.buttonRed.Click += new System.EventHandler(this.buttonRed_Click);
+            // 
+            // buttonGreen
+            // 
+            this.buttonGreen.BackColor = System.Drawing.Color.Green;
+            this.buttonGreen.Location = new System.Drawing.Point(627, 17);
+            this.buttonGreen.Name = "buttonGreen";
+            this.buttonGreen.Size = new System.Drawing.Size(27, 23);
+            this.buttonGreen.TabIndex = 4;
+            this.buttonGreen.UseVisualStyleBackColor = false;
+            this.buttonGreen.Click += new System.EventHandler(this.buttonGreen_Click);
+            // 
+            // buttonYellow
+            // 
+            this.buttonYellow.BackColor = System.Drawing.Color.Yellow;
+            this.buttonYellow.Location = new System.Drawing.Point(660, 17);
+            this.buttonYellow.Name = "buttonYellow";
+            this.buttonYellow.Size = new System.Drawing.Size(27, 23);
+            this.buttonYellow.TabIndex = 5;
+            this.buttonYellow.UseVisualStyleBackColor = false;
+            this.buttonYellow.Click += new System.EventHandler(this.buttonYellow_Click);
+            // 
+            // buttonBlack
+            // 
+            this.buttonBlack.BackColor = System.Drawing.Color.Black;
+            this.buttonBlack.Location = new System.Drawing.Point(693, 17);
+            this.buttonBlack.Name = "buttonBlack";
+            this.buttonBlack.Size = new System.Drawing.Size(27, 23);
+            this.buttonBlack.TabIndex = 6;
+            this.buttonBlack.UseVisualStyleBackColor = false;
+            this.buttonBlack.Click += new System.EventHandler(this.buttonBlack_Click);
+            // 
+            // buttonLightBlue
+            // 
+            this.buttonLightBlue.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.buttonLightBlue.Location = new System.Drawing.Point(559, 46);
+            this.buttonLightBlue.Name = "buttonLightBlue";
+            this.buttonLightBlue.Size = new System.Drawing.Size(27, 23);
+            this.buttonLightBlue.TabIndex = 7;
+            this.buttonLightBlue.UseVisualStyleBackColor = false;
+            this.buttonLightBlue.Click += new System.EventHandler(this.buttonLightBlue_Click);
+            // 
+            // buttonOrange
+            // 
+            this.buttonOrange.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonOrange.Location = new System.Drawing.Point(592, 46);
+            this.buttonOrange.Name = "buttonOrange";
+            this.buttonOrange.Size = new System.Drawing.Size(27, 23);
+            this.buttonOrange.TabIndex = 8;
+            this.buttonOrange.UseVisualStyleBackColor = false;
+            this.buttonOrange.Click += new System.EventHandler(this.buttonOrange_Click);
+            // 
+            // buttonLightGreen
+            // 
+            this.buttonLightGreen.BackColor = System.Drawing.Color.Lime;
+            this.buttonLightGreen.Location = new System.Drawing.Point(625, 46);
+            this.buttonLightGreen.Name = "buttonLightGreen";
+            this.buttonLightGreen.Size = new System.Drawing.Size(27, 23);
+            this.buttonLightGreen.TabIndex = 9;
+            this.buttonLightGreen.UseVisualStyleBackColor = false;
+            this.buttonLightGreen.Click += new System.EventHandler(this.buttonLightGreen_Click);
+            // 
+            // buttonPink
+            // 
+            this.buttonPink.BackColor = System.Drawing.Color.Fuchsia;
+            this.buttonPink.Location = new System.Drawing.Point(660, 46);
+            this.buttonPink.Name = "buttonPink";
+            this.buttonPink.Size = new System.Drawing.Size(27, 23);
+            this.buttonPink.TabIndex = 10;
+            this.buttonPink.UseVisualStyleBackColor = false;
+            this.buttonPink.Click += new System.EventHandler(this.buttonPink_Click);
+            // 
+            // buttonWhite
+            // 
+            this.buttonWhite.BackColor = System.Drawing.Color.White;
+            this.buttonWhite.Location = new System.Drawing.Point(693, 46);
+            this.buttonWhite.Name = "buttonWhite";
+            this.buttonWhite.Size = new System.Drawing.Size(27, 23);
+            this.buttonWhite.TabIndex = 11;
+            this.buttonWhite.UseVisualStyleBackColor = false;
+            this.buttonWhite.Click += new System.EventHandler(this.buttonWhite_Click);
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(537, 19);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(22, 20);
+            this.textBoxWidth.TabIndex = 12;
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWidth.Location = new System.Drawing.Point(490, 22);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(41, 13);
+            this.labelWidth.TabIndex = 13;
+            this.labelWidth.Text = " width";
+            this.labelWidth.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonErase
+            // 
+            this.buttonErase.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.buttonErase.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonErase.Location = new System.Drawing.Point(47, 22);
+            this.buttonErase.Name = "buttonErase";
+            this.buttonErase.Size = new System.Drawing.Size(47, 28);
+            this.buttonErase.TabIndex = 14;
+            this.buttonErase.Text = "reset";
+            this.buttonErase.UseVisualStyleBackColor = false;
+            this.buttonErase.Click += new System.EventHandler(this.buttonErase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonErase);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.buttonWhite);
+            this.Controls.Add(this.buttonPink);
+            this.Controls.Add(this.buttonLightGreen);
+            this.Controls.Add(this.buttonOrange);
+            this.Controls.Add(this.buttonLightBlue);
+            this.Controls.Add(this.buttonBlack);
+            this.Controls.Add(this.buttonYellow);
+            this.Controls.Add(this.buttonGreen);
+            this.Controls.Add(this.buttonRed);
+            this.Controls.Add(this.buttonBLue);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonBLue;
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonGreen;
+        private System.Windows.Forms.Button buttonYellow;
+        private System.Windows.Forms.Button buttonBlack;
+        private System.Windows.Forms.Button buttonLightBlue;
+        private System.Windows.Forms.Button buttonOrange;
+        private System.Windows.Forms.Button buttonLightGreen;
+        private System.Windows.Forms.Button buttonPink;
+        private System.Windows.Forms.Button buttonWhite;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Button buttonErase;
+    }
+}
+
---
+++ malovani/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace malovani
+{
+    public partial class Form1 : Form
+    {
+        public Point current = new Point();
+        public Point old = new Point();
+        public Graphics g;
+        public Graphics graph;
+        public Pen pen = new Pen(Color.Black, 3);
+        Bitmap surface;
+        public Form1()
+
+        {
+            InitializeComponent();
+            
+            g = flowLayoutPanel1.CreateGraphics();
+
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+
+        }
+
+        private void flowLayoutPanel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            old = e.Location;
+        }
+
+        private void flowLayoutPanel1_MouseHover(object sender, EventArgs e)
+        {
+        }
+
+        private void flowLayoutPanel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                current = e.Location;
+                g.DrawLine(pen, old, current);
+                old = current;
+            }
+
+        }
+
+        private void buttonBLue_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Blue;
+        }
+
+        private void buttonRed_Click(object sender, EventArgs e)
+        {
+            pen.Color= Color.Red;
+        }
+
+        private void buttonGreen_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Green;
+        }
+
+        private void buttonYellow_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Yellow;
+        }
+
+        private void buttonBlack_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Black;
+        }
+
+        private void buttonLightBlue_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.LightSkyBlue;
+        }
+
+        private void buttonOrange_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Orange;
+        }
+
+        private void buttonLightGreen_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Lime;
+        }
+
+        private void buttonPink_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.Fuchsia;
+        }
+
+        private void buttonWhite_Click(object sender, EventArgs e)
+        {
+            pen.Color = Color.White;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int i = 3;
+            int test = 0;
+            while (test == 0)
+            {
+                try
+                {
+                    i = int.Parse(textBoxWidth.Text);
+                    test = 1;
+                }
+                catch
+                {
+                    test = 0;
+                }
+            }
+              
+            pen.Width = i;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonErase_Click(object sender, EventArgs e)
+        {
+            g.FillRectangle(Brushes.White, 0, 0, flowLayoutPanel1.Width, flowLayoutPanel1.Height);
+        }
+    }
+}
